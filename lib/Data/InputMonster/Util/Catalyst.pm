c6d4f8ad0a7ea732a909f8d56ef482a54a63dc1f
Ricardo SIGNES <rjbs@cpan.org>
2013-07-06 01:41:12 +0000

    Import of RJBS/Data-InputMonster-Util-Catalyst-0.004 from CPAN.
    gitpan-cpan-distribution: Data-InputMonster-Util-Catalyst
    gitpan-cpan-version:      0.004
    gitpan-cpan-path:         RJBS/Data-InputMonster-Util-Catalyst-0.004.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Data/InputMonster/Util/Catalyst.pm
+++ lib/Data/InputMonster/Util/Catalyst.pm
@@ -1,8 +1,9 @@
 use strict;
 use warnings;
 package Data::InputMonster::Util::Catalyst;
-our $VERSION = '0.003';
-
+{
+  $Data::InputMonster::Util::Catalyst::VERSION = '0.004';
+}
 # ABSTRACT: InputMonster sources for common Catalyst sources
 
 
@@ -69,7 +70,7 @@ Data::InputMonster::Util::Catalyst - InputMonster sources for common Catalyst so
 
 =head1 VERSION
 
-version 0.003
+version 0.004
 
 =head1 DESCRIPTION
 
@@ -84,43 +85,41 @@ C<$input> argument to the monster's C<consume> method.
 
 =head2 form_param
 
-    my $source = form_param($field_name);
+  my $source = form_param($field_name);
 
 This source will look for form parameters (with C<< $c->req->params >>) with
 the given field name.
 
 =head2 body_param
 
-    my $source = body_param($field_name);
+  my $source = body_param($field_name);
 
 This source will look for form parameters (with C<< $c->req->body_params >>)
 with the given field name.
 
 =head2 query_param
 
-    my $source = query_param($field_name);
+  my $source = query_param($field_name);
 
 This source will look for form parameters (with C<< $c->req->query_params >>)
 with the given field name.
 
 =head2 session_entry
 
-    my $source = session_entry($locator);
+  my $source = session_entry($locator);
 
 This source will look for an entry in the session for the given locator, using
 the C<dig> utility from L<Data::InputMonster::Util>.
 
 =head1 AUTHOR
 
-  Ricardo SIGNES <rjbs@cpan.org>
+Ricardo SIGNES <rjbs@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2008 by Ricardo SIGNES.
+This software is copyright (c) 2013 by Ricardo SIGNES.
 
 This is free software; you can redistribute it and/or modify it under
-the same terms as perl itself.
+the same terms as the Perl 5 programming language system itself.
 
-=cut 
-
-
+=cut
